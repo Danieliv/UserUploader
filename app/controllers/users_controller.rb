3938dc2e672e18d9ff9a75fd51471c4f11c6db84
Daniel Navarrete <11926357+Danieliv@users.noreply.github.com>
2024-08-24 00:49:59 -0600

    Add logic to handle CSV file upload and user creation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
-  def index
-  end
+  def index; end
 
   def upload
+    @results = []
+
+    if params[:file].present?
+      CSV.foreach(params[:file].path, headers: true) do |row|
+        user = User.new(name: row['name'], password: row['password'])
+        if user.save
+          @results << { name: row['name'], result: 'Success' }
+        else
+          @results << { name: row['name'], result: "Error: #{user.errors.full_messages.join(', ')}" }
+        end
+      end
+    else
+      @results << { error: "No file uploaded" }
+    end
+
+    render :index
   end
 end
