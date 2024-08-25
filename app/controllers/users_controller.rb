e55d44ae58a568bb9788262edf3c51e6ee2a719f
Daniel Navarrete <11926357+Danieliv@users.noreply.github.com>
2024-08-25 15:43:39 -0600

    Update UserController to use the new service for processing the CSV file.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,7 @@ class UsersController < ApplicationController
   def index; end
 
   def upload
-    @results = []
-
-    if params[:file].present?
-      CSV.foreach(params[:file].path, headers: true) do |row|
-        user = User.new(name: row['name'], password: row['password'])
-        if user.save
-          @results << { name: row['name'], result: 'Success' }
-        else
-          @results << { name: row['name'], result: "Error: #{user.errors.full_messages.join(', ')}" }
-        end
-      end
-    else
-      @results << { error: "No file uploaded" }
-    end
-
-    render :index
+    results = CsvUploaderService.new(params[:file]).process
+    render json: { results: results }
   end
 end
